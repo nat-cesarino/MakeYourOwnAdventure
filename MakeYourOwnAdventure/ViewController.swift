c186951badabe2cbc05a70c8bc50c328c7eb5711
Nathalie Pires Cesarino <nathalie.cesarino@kroton.tech>
2022-01-12 18:58:22 -0300

    Added start over button that leads back to root view controller

--- MakeYourOwnAdventure/ViewController.swift
+++ MakeYourOwnAdventure/ViewController.swift
@@ -11,9 +11,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Start Over", style: .plain, target: self, action: #selector(startOver))
     }
-
-
+    
+    @objc func startOver() {
+        if let navigationController = navigationController {
+            navigationController.popToRootViewController(animated: true)
+        }
+    }
 }
 
